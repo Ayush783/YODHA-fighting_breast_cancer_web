701f201d2007107cb4f3525ec01d6422156e82a8
Ayush783 <ayushc45xxx@gmail.com>
2021-05-12 15:47:04 +0530

    made sign in page ui

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 from api import views
 
 urlpatterns = [
-    path('/', views.apihome, name = 'apihome'),
-    path('/signup', views.signup, name = 'signup'),
+    path('', views.apihome, name = 'apihome'),
+    path('signup', views.signup, name = 'signup'),
 ]
